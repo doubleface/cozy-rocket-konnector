792fb016d330584cfbfafcbe1af55ca61f20de9a
doubleface <christophe@thiriot.xyz>
2017-03-23 18:04:29 +0100

    Now credentials and gozy url are read from env
    COZY_CREDENTIALS and COZY_URL

--- konnector/lib/cozyclient.js
+++ konnector/lib/cozyclient.js
@@ -1,17 +1,29 @@
 const {Client, MemoryStorage} = require('cozy-client-js')
 
-const token = JSON.parse(require("fs").readFileSync(__dirname + "/../token.json", "utf-8"))
+// COZY_CREDENTIALS
+let credentials = null
+try {
+  credentials = JSON.parse(process.env.COZY_CREDENTIALS)
+} catch (err) {
+  console.log(`Please provide proper COZY_CREDENTIALS environment variable. ${process.env.COZY_CREDENTIALS} is not OK`)
+  process.exit(1)
+}
 
-token.token.toAuthHeader = function() {
-  return 'Bearer ' + token.client.registrationAccessToken
+// COZY_URL
+if (process.env.COZY_URL === undefined) {
+  console.log(`Please provide COZY_URL environment variable.`)
+  process.exit(1)
+}
+
+credentials.token.toAuthHeader = function () {
+  return 'Bearer ' + credentials.client.registrationAccessToken
 }
 
 const cozy = new Client({
-  cozyURL: 'http://cozy.local:8080',
+  cozyURL: process.env.COZY_URL,
   oauth: {storage: new MemoryStorage()}
 })
 
-cozy.saveCredentials(token.client, token.token)
+cozy.saveCredentials(credentials.client, credentials.token)
 
 module.exports = cozy
-
